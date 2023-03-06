1feaa88598c635961687e82dee5b29086e646957
Julian Flores <julianfloresdev@gmail.com>
2023-03-06 10:55:19 -0300

    feat: add private route component

--- src/components/CounterForTest/index.jsx
+++ src/components/CounterForTest/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { decrementBy, incrementBy } from '../../redux/global/actions';
 
 const CounterForTest = () => {
@@ -8,45 +9,53 @@ const CounterForTest = () => {
   const [numberOfIncrement, setNumberOfIncrement] = useState(1);
   const [numberOfDecrement, setNumberOfDecrement] = useState(1);
   return (
-    <div className="w-screen h-screen bg-black flex flex-wrap justify-center items-center gap-3">
-      <div className="flex gap-2 border-2 border-indigo-400 px-4 py-2 rounded-2xl">
-        <label htmlFor="incrementRange">primero</label>
-        <input
-          type="range"
-          id="incrementRange"
-          min={1}
-          max={100}
-          value={numberOfIncrement}
-          onChange={(e) => setNumberOfIncrement(Number(e.target.value))}
-        />
-        <button
-          onClick={() => dispatch(incrementBy(numberOfIncrement))}
-          className="bg-primary-200 hover:bg-primary-700 px-4 py-2 rounded font-bold text-xl"
+    <>
+      <div className="w-screen h-screen bg-black flex flex-wrap justify-center items-center gap-3">
+        <div className="flex gap-2 border-2 border-indigo-400 px-4 py-2 rounded-2xl">
+          <label htmlFor="incrementRange">primero</label>
+          <input
+            type="range"
+            id="incrementRange"
+            min={1}
+            max={100}
+            value={numberOfIncrement}
+            onChange={(e) => setNumberOfIncrement(Number(e.target.value))}
+          />
+          <button
+            onClick={() => dispatch(incrementBy(numberOfIncrement))}
+            className="bg-primary-200 hover:bg-primary-700 px-4 py-2 rounded font-bold text-xl"
+          >
+            Increment {numberOfIncrement}
+          </button>
+        </div>
+        <div className="flex gap-2 border-2 border-indigo-400 px-4 py-2 rounded-2xl">
+          <label htmlFor="decrementRange">segundo</label>
+          <input
+            type="range"
+            id="decrementRange"
+            min={1}
+            max={100}
+            value={numberOfDecrement}
+            onChange={(e) => setNumberOfDecrement(Number(e.target.value))}
+          />
+          <button
+            onClick={() => dispatch(decrementBy(numberOfDecrement))}
+            className="bg-primary-200 hover:bg-primary-700 px-4 py-2 rounded font-bold text-xl"
+          >
+            Decrement {numberOfDecrement}
+          </button>
+        </div>
+        <p className="px-4 py-2 bg-tomato-200 border-2 border-tomato-700 rounded-xl font-extrabold text-xl text-slate-600">
+          {counter}
+        </p>
+        <Link
+          to={'/private'}
+          className="px-4 py-2 rounded-md border-2 border-orange"
         >
-          Increment {numberOfIncrement}
-        </button>
+          Navigate to a Private Route
+        </Link>
       </div>
-      <div className="flex gap-2 border-2 border-indigo-400 px-4 py-2 rounded-2xl">
-        <label htmlFor="decrementRange">segundo</label>
-        <input
-          type="range"
-          id="decrementRange"
-          min={1}
-          max={100}
-          value={numberOfDecrement}
-          onChange={(e) => setNumberOfDecrement(Number(e.target.value))}
-        />
-        <button
-          onClick={() => dispatch(decrementBy(numberOfDecrement))}
-          className="bg-primary-200 hover:bg-primary-700 px-4 py-2 rounded font-bold text-xl"
-        >
-          Decrement {numberOfDecrement}
-        </button>
-      </div>
-      <p className="px-4 py-2 bg-tomato-200 border-2 border-tomato-700 rounded-xl font-extrabold text-xl text-slate-600">
-        {counter}
-      </p>
-    </div>
+    </>
   );
 };
 
