76d9ab99c8230f4ef1a505439db1104155d64d6d
Julian Flores <julianfloresdev@gmail.com>
2023-03-06 15:00:31 -0300

    feat: remove button from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Button from './components/Button';
 import CounterForTest from './components/CounterForTest';
 
 function App() {
   return (
     <>
       <CounterForTest />
-      <Button bgColor='green' action={() => {}} solid={true}></Button>
     </>
   );
 }
