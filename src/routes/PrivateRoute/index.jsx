c89058dc9e6cad8de075b923e9aace17451beeb7
Julian Flores <julianfloresdev@gmail.com>
2023-03-06 15:04:38 -0300

    fix: add vercel.json and fix URL rendering

--- src/routes/PrivateRoute/index.jsx
+++ src/routes/PrivateRoute/index.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router';
 
 const PrivateRoute = () => {
   //This conditional should be from authentication prop or validation block
-  const isAuthenticated = false;
+  const isAuthenticated = true;
   return isAuthenticated ? <Outlet /> : <Navigate to={'/'} />;
 };
 
