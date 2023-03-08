4d7935a5d9ae4a310705101d0475cc18f639f457
Julian Flores <julianfloresdev@gmail.com>
2023-03-07 21:59:07 -0300

    feat: add comments recomendations

--- src/components/Button/button.test.js
+++ src/components/Button/button.test.js
@@ -1,15 +1,15 @@
 import Button from '.';
 import { fireEvent, render, screen } from '../../utils/tests/renderWithRouter';
 
-const handleClick = (event) => {
-  event.target.textContent = 'Clicked';
-};
+const onCLick = jest.fn();
 
 describe('Button Tests', () => {
   it('Should render correctly', () => {
     render(<Button>Press ME</Button>);
+    //Check if button is rendered on mock DOM
     const button = screen.getByRole('button', { name: /press me/i });
     expect(button).toBeInTheDocument();
+    //Test if button have default background color when it's not define in props
     expect(button).toHaveClass('bg-primary-700');
   });
   it('Should render incorrectly when search for value "press me"', () => {
@@ -20,21 +20,21 @@ describe('Button Tests', () => {
   });
   it('Should have background color orange, when pass "bgColor" property as orange value', () => {
     render(<Button bgColor="orange">Click here</Button>);
+    //Test if backgound color is the called as bgColor prop and children prop show correctly
     expect(screen.getByRole('button', { name: /click here/i })).toHaveClass(
       'bg-orange-700'
     );
   });
   it('Should change the button value when fire a click', () => {
     render(
-      <Button bgColor="orange" onClick={handleClick}>
+      <Button bgColor="orange" onClick={onCLick}>
         Click Here
       </Button>
     );
     const button = screen.getByRole('button', { name: /click here/i });
     expect(button).toBeInTheDocument();
+    //Test button click event is correctly working on
     fireEvent.click(button);
-    expect(
-      screen.getByRole('button', { name: /clicked/i })
-    ).toBeInTheDocument();
+    expect(onCLick).toHaveBeenCalled();
   });
 });
