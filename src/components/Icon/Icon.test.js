c7b566a7a0fd8ed1cde8885205c2f0688bde4381
Damian Palavecino <damianpalavecino25@gmail.com>
2023-03-07 23:32:57 -0300

    test: create tests for Icon component

---
+++ src/components/Icon/Icon.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import Icon from './index';
+
+describe('Icon', () => {
+  it('The icon container and the icon img are rendered correctly', () => {
+    render(<Icon icon="alert" />);
+    const iconContainer = screen.getByRole('img').parentElement;
+    const icon = screen.getByRole('img');
+    expect(icon).toBeInTheDocument();
+    expect(iconContainer).toBeInTheDocument();
+  });
+  it('Icon renders correctly with the right src attribute', () => {
+    render(<Icon icon="alert" />);
+    const icon = screen.getByRole('img');
+    expect(icon).toBeInTheDocument();
+    expect(icon).toHaveAttribute('src', '/assets/icons/alert.svg');
+  });
+
+  it('Icon container renders correctly with bigCircle container type', () => {
+    render(<Icon icon="alert" containerType="bigCircle" />);
+    const iconContainer = screen.getByRole('img').parentElement;
+    expect(iconContainer).toBeInTheDocument();
+    const bigCircleStyles = 'flex justify-center items-center bg-white w-[80px] h-[80px] rounded-full';
+    expect(iconContainer).toHaveClass(bigCircleStyles);
+  });
+
+  it('renders correctly with circle container type', () => {
+    render(<Icon icon="alert" containerType="circle" />);
+    const iconContainer = screen.getByRole('img').parentElement;
+    expect(iconContainer).toBeInTheDocument();
+    const circleStyles = 'flex justify-center items-center bg-white w-14 h-14 rounded-full';
+    expect(iconContainer).toHaveClass(circleStyles);
+  });
+
+  it('renders correctly with square container type', () => {
+    render(<Icon icon="alert" containerType="square" />);
+    const iconContainer = screen.getByRole('img').parentElement;
+    expect(iconContainer).toBeInTheDocument();
+    const squareStyles = 'flex justify-center items-center bg-white w-[80px] h-[80px] rounded-lg border border-black';
+    expect(iconContainer).toHaveClass(squareStyles);
+    expect(screen.getByRole('img')).toHaveClass('w-12');
+  });
+});
