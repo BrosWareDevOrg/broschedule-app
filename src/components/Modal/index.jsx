c70c7d043cb46cdf3b0611d410c7d0e6b3567ba8
Julian Flores <julianfloresdev@gmail.com>
2023-03-13 11:07:49 -0300

    feat: add click outside modal to close it

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setModalState } from '../../redux/global/actions';
 
@@ -9,17 +9,58 @@ const Modal = ({
 }) => {
   const { isModalActive } = useSelector((store) => store.global);
   const dispatch = useDispatch();
-
-  const handleEscKeyEvent = (e) => {
-    (e.key === 'Escape' || e.key === 'Esc') && dispatch(setModalState(false));
-  };
+  const modalBackground = useRef(null);
+  const modalContent = useRef(null);
 
   useEffect(() => {
+    const handleEscKeyEvent = (e) => {
+      //Function to handle 'Escape' keypress event
+      (e.key === 'Escape' || e.key === 'Esc') && dispatch(setModalState(false));
+    };
+    //Adding keypress event
     document.addEventListener('keyup', handleEscKeyEvent);
     return () => {
+      //Removing 'Escape' keypress event
       document.removeEventListener('keyup', handleEscKeyEvent);
     };
   }, []);
+
+  const addClickEventListener = () => {
+    modalBackground.current &&
+      modalBackground.current.addEventListener(
+        'click',
+        handleClickToCloseModal
+      );
+  };
+
+  useEffect(() => {
+    addClickEventListener();
+    return () => {
+      modalBackground.current &&
+        modalBackground.current.removeEventListener(
+          'click',
+          handleClickToCloseModal
+        );
+    };
+  }, [modalBackground.current]);
+
+  const handleClickToCloseModal = (e) => {
+    //Function to handle click on background rect event
+    const { clientHeight: contentHeight, clientWidth: contentWidth } =
+      modalContent.current;
+    const { clientX: x, clientY: y } = e;
+    const { innerHeight: totalHeight, innerWidth: totalWidth } = window;
+
+    const paddingX = (totalWidth - contentWidth) / 2;
+    const paddingY = (totalHeight - contentHeight) / 2;
+
+    if (
+      !(x > paddingX && x < paddingX + contentWidth) ||
+      !(y > paddingY && y < paddingY + contentHeight)
+    ) {
+      dispatch(setModalState(false));
+    }
+  };
 
   const styles = {
     xmark: 'p-1 w-9 h-9 cursor-pointer rounded-md hover:bg-primary-200',
@@ -38,8 +79,9 @@ const Modal = ({
   return (
     <Suspense fallback={<div>Loading. . .</div>}>
       {!isModalActive ? null : (
-        <div className={styles.container}>
+        <div ref={modalBackground} className={styles.container}>
           <div
+            ref={modalContent}
             className={`${styles.content.display} ${styles.content.font} ${styles.content.background}`}
           >
             {sections?.map((section, index) => {
