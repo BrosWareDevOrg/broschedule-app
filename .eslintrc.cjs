1d15a6eec8a17d0c69f90281f65f5df85c4146d8
Julian Flores <julianfloresdev@gmail.com>
2023-03-14 09:55:57 -0300

    feat: config ESlint rules and plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,11 @@ module.exports = {
     // By extending from a plugin config, we can get recommended rules without having to add them manually.
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:jest-dom/recommended',
     // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
     // Make sure it's always the last config, so it gets the chance to override other configs.
     'eslint-config-prettier',
+    'plugin:testing-library/react',
   ],
   settings: {
     react: {
