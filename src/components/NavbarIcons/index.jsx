e83fac5be10bc413541d5055f74db2f6c921e444
Julian Flores <julianfloresdev@gmail.com>
2023-03-23 20:25:17 -0300

    fix: styling hover icon effect

--- src/components/NavbarIcons/index.jsx
+++ src/components/NavbarIcons/index.jsx
@@ -1,31 +1,20 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import SVGExporter from '../../utils/functions/SVGExport';
 
 const NavbarIcon = ({ icon = 'notifications' }) => {
   const location = useLocation();
   const currentLocation = location.pathname;
-  const [isHovering, setHoveringState] = useState(false);
   const svgProps = SVGExporter(icon);
   return (
-    <div
-      className="flex items-center justify-center w-8 h-8 p-1 md:w-16 md:h-16 md:p-4 md:rounded-md md:hover:bg-gray-bg-500 md:cursor-pointer transition-all"
-      onMouseOver={() => setHoveringState(true)}
-      onMouseLeave={() => setHoveringState(false)}
-    >
+    <div className="group flex items-center justify-center w-8 h-8 p-1 md:w-16 md:h-16 md:p-4 md:rounded-md md:hover:bg-gray-bg-500 md:cursor-pointer transition-all">
       <svg
+        className="group-hover:fill-primary-700"
         width={svgProps.width}
         height={svgProps.height}
         viewBox={svgProps.viewBox}
         xmlns="http://www.w3.org/2000/svg"
-        fill={
-          isHovering
-            ? '#8359E3'
-            : currentLocation.includes(icon)
-            ? 'white'
-            : '#C0C0C080'
-        }
+        fill={currentLocation.includes(icon) ? 'white' : '#C0C0C080'}
       >
         {svgProps.paths.map((path, index) => {
           return <path d={path} key={index} fillOpacity="0.6" />;
