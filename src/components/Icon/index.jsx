89b3a5034a96754bb731c0c7f3d7fcab0b5679c1
Ernesto Bessone <ejubessone@gmail.com>
2023-03-24 16:27:27 -0300

    style: Icon styles modified for the Service Card

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const icons = [
+export const icons = [
   'alert',
   'ball',
   'dark',
@@ -19,12 +19,12 @@ const Icon = ({ icon, containerType }) => {
   const containerStyles = {
     circle: 'bg-white w-14 h-14 rounded-full',
     bigCircle: 'bg-white w-[80px] h-[80px] rounded-full',
-    square: 'bg-white w-[80px] h-[80px] rounded-lg border border-black',
+    square: 'bg-white w-[118px] h-[118px] rounded-lg border border-black',
   };
 
   return (
     <div className={'flex justify-center items-center ' + containerStyles[containerType] || ''}>
-      <img src={`/assets/icons/${icon}.svg`} className={containerType === 'square' ? 'w-12' : 'w-8'} />
+      <img src={`/assets/icons/${icon}.svg`} className={containerType === 'square' ? 'w-3/4' : 'w-8'} alt="icon"/>
     </div>
   );
 };
