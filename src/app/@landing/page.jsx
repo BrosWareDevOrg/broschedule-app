af8340c5be778e906835edce4e4b3c26ca5e6f21
Julian Flores <julianfloresdev@gmail.com>
2023-04-18 15:59:48 -0300

    feat: create diferent pages to render landing and app

---
+++ src/app/@landing/page.jsx
@@ -0,0 +1,12 @@
+const LandingPage = () => {
+  return (
+    <main className="w-screen flex flex-col scrollbar-hide">
+      <section className="w-full h-screen bg-primary-700"></section>
+      <section className="w-full h-screen bg-white"></section>
+      <section className="w-full h-screen bg-primary-700"></section>
+      <section className="w-full h-screen bg-white"></section>
+    </main>
+  );
+};
+
+export default LandingPage;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,6 @@
-import '@/styles/globals.css';
+import './globals.css';
+
+import { cookies } from 'next/headers';
 
 import Providers from './ReduxContext/provider';
 
@@ -9,15 +11,24 @@ export const metadata = {
   description: 'Improve the way to contact services in our city.',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, landing }) {
+  //One way to handle session data is with next 'cookies' function, which use is similar to 'document.coockie'
+  //We can choice to use other methods mor specific in future to handle users and auths, like firebase hooks.
+  const coockieList = cookies();
   return (
     <html>
       <head />
       <body>
         <Providers>
-          <main className="relative flex flex-col-reverse md:flex-row w-screen max-w-[100vw] h-screen">
-            <Navbar />
-            {children}
+          <main className="relative w-screen max-w-[100vw] h-screen md:h-fit bg-white">
+            {coockieList.has('userToken') ? (
+              <>
+                <Navbar />
+                {children}
+              </>
+            ) : (
+              landing
+            )}
           </main>
         </Providers>
       </body>
