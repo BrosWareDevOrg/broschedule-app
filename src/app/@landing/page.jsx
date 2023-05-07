108ce2893d0be6592e9b641e14189a43ed4a485c
Julian Flores <julianfloresdev@gmail.com>
2023-05-07 13:38:32 -0300

    feat: add modify from deleted branch

--- src/app/@landing/page.jsx
+++ src/app/@landing/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Button from '@/components/Button';
 import NavbarIcon from '@/components/NavbarIcons';
 
-const LandingPage = () => {
+export default function LandingPage() {
   return (
     <main className="w-screen flex flex-col scrollbar-hide bg-white">
       <header className="w-full px-6 py-2 flex items-center drop-shadow-md">
@@ -10,11 +10,11 @@ const LandingPage = () => {
           <ul className="w-full flex justify-between items-center drop-shadow-md">
             <li>
               <Link href={'/'}>
-                <NavbarIcon icon='logo' />
+                <NavbarIcon icon="logo" />
               </Link>
             </li>
             <li>
-              <ul className='flex gap-4'>
+              <ul className="flex gap-4">
                 <li>
                   <Link href={'/auth/login'}>
                     <Button bgColor="primary" isSolid={true}>
@@ -40,6 +40,4 @@ const LandingPage = () => {
       <article className="w-full h-screen"></article>
     </main>
   );
-};
-
-export default LandingPage;
+}
